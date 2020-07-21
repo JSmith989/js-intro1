adf88a533e2e6f8bd052413070516ee6dcd7c9d8
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-07-20 19:57:04 -0500

    methods

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("logging")
+
+
+let x = true;
+let y = false;
+
+
+// console.log(x, y)
+
+
+
+/* if (x) {
+    console.log("this is true")
+} else {
+    console.log("this is false")
+} */
+
+
